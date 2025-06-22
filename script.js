b4cc00898e2583fc7fd7a777f4b7082563f1607a
hatem07 <hatemghouthi@yahoo.com>
2025-06-22 10:12:00 +0100

    Update script.js
    add cn ar

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // List of TV channels with their .m3u8 links
 const channels = [
     { name: "Mbc3 144p", url: "https://shls-mbc3-prod-dub.shahid.net/out/v1/d5bbe570e1514d3d9a142657d33d85e6/index_8.m3u8" },
-	{ name: "MBC3 240p", url: "https://shls-mbc3-prod-dub.shahid.net/out/v1/d5bbe570e1514d3d9a142657d33d85e6/index_10.m3u8" },
+    { name: "MBC3 240p", url: "https://shls-mbc3-prod-dub.shahid.net/out/v1/d5bbe570e1514d3d9a142657d33d85e6/index_10.m3u8" },
     { name: "MBC3 360p", url: "https://shls-mbc3-prod-dub.shahid.net/out/v1/d5bbe570e1514d3d9a142657d33d85e6/index_5.m3u8" },
-	{ name: "Spacetoon 240p", url: "https://streams.spacetoon.com/live/stchannel/smil:livesmil.smil/chunklist_w810512056_b346000_slAR.m3u8" },
-	{ name: "Spacetoon 360p", url: "https://streams.spacetoon.com/live/stchannel/smil:livesmil.smil/chunklist_w810512056_b596000_slAR.m3u8" },
-	{ name: "Spacetoon 480p", url: "https://streams.spacetoon.com/live/stchannel/smil:livesmil.smil/chunklist_w810512056_b946000_slAR.m3u8" },
-	{ name: "Spacetoon auto", url: "https://streams.spacetoon.com/live/stchannel/smil:livesmil.smil/playlist.m3u8" },
+    { name: "Spacetoon 240p", url: "https://streams.spacetoon.com/live/stchannel/smil:livesmil.smil/chunklist_w810512056_b346000_slAR.m3u8" },
+    { name: "Spacetoon 360p", url: "https://streams.spacetoon.com/live/stchannel/smil:livesmil.smil/chunklist_w810512056_b596000_slAR.m3u8" },
+    { name: "Spacetoon 480p", url: "https://streams.spacetoon.com/live/stchannel/smil:livesmil.smil/chunklist_w810512056_b946000_slAR.m3u8" },
+    { name: "Spacetoon auto", url: "https://streams.spacetoon.com/live/stchannel/smil:livesmil.smil/playlist.m3u8" },
+    { name: "Cn AR auto", url: "https://watch.3rbcafee.com/2024/10/cnarabia.html" }
 	/*{ name: "", url: "" },
 	{ name: "", url: "" },
 	{ name: "", url: "" },
